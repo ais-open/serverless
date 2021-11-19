3ad8029d71ba281b1257c12c172847ab27f86e73
Tom McKearney <tom.mckearney@ais.com>
2021-11-19 14:18:56 -0500

    adding more monitoring demo code

--- Orchestrator/FindFaceCount.cs
+++ Orchestrator/FindFaceCount.cs
@@ -8,30 +8,32 @@ using Newtonsoft.Json;
 
 namespace Live360.Demo.Orchestration
 {
-  public static class FindFaceCount
-  {
-    /// <summary>
-    /// Finds the face count in an image
-    /// </summary>
-    /// <param name="inputs">the input to the operation</param>
-    /// <param name="logger">the logger</param>
-    /// <returns>the count of detected faces</returns>
-    [FunctionName(nameof(FindFaceCount))]
-    public static int Run(
-      [ActivityTrigger] ReceivedImageInfo input,
-      ILogger logger)
+    public static class FindFaceCount
     {
-        // Take up some memory
-        var bytes = new byte[30_000_000];
+        /// <summary>
+        /// Finds the face count in an image
+        /// </summary>
+        /// <param name="inputs">the input to the operation</param>
+        /// <param name="logger">the logger</param>
+        /// <returns>the count of detected faces</returns>
+        [FunctionName(nameof(FindFaceCount))]
+        public static int Run(
+          [ActivityTrigger] ReceivedImageInfo input,
+          ILogger logger)
+        {
+            if (Random.Shared.Next(10) == 0) throw new Exception("Buggy Library Exception");
 
-        logger.LogInformation("Finding Face Count For Image: {Image}", JsonConvert.SerializeObject(input));
-        
-        // Simulate retrieving and processing the image
-        Thread.Sleep(Random.Shared.Next(10_000, 30_000));
+            // Take up some memory
+            var bytes = new byte[30_000_000];
 
-        // fake results
-        var faceCount = Random.Shared.Next(0, 5);
-        return faceCount;
+            logger.LogInformation("Finding Face Count For Image: {Image}", JsonConvert.SerializeObject(input));
+
+            // Simulate retrieving and processing the image
+            Thread.Sleep(Random.Shared.Next(10_000, 30_000));
+
+            // fake results
+            var faceCount = Random.Shared.Next(0, 5);
+            return faceCount;
+        }
     }
-  }
 }
--- Orchestrator/SendNotifications.cs
+++ Orchestrator/SendNotifications.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Live360.Demo.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
@@ -8,19 +11,26 @@ namespace Live360.Demo.Orchestration
 {
   public static class SendNotifications
   {
+
+    private static HttpClient httpClient = new HttpClient();
+    
     /// <summary>
     /// Send notifications about the results
     /// </summary>
     /// <param name="inputs">the input to the operation</param>
     /// <param name="logger">the logger</param>
     [FunctionName(nameof(SendNotifications))]
-    public static void Run(
+    public static async Task Run(
       [ActivityTrigger] ReceivedImageInfo input,
       ILogger logger)
     {
         // TODO: Send Notification
 
         logger.LogInformation("Sending Notification for Image: {Image}", JsonConvert.SerializeObject(input));
+
+        // pretend 'Hello' is an external notification API call
+        var hostname = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
+        await httpClient.GetAsync($"https://{hostname}/api/Hello");
     }
   }
 }
